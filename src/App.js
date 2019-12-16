693425a00331971fea7e01ba931b29127058094d
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-12-16 05:35:39 +0100

    Add gallery for portrait images

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter } from 'react-router-dom';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import { GlobalStyles } from './components/styled-components/GlobalStyles';
+import Portraits from './components/Portraits';
 
 export default function App() {
   return (
     <BrowserRouter>
       <GlobalStyles />
       <Header />
+      <Portraits />
       <Footer />
     </BrowserRouter>
   );
