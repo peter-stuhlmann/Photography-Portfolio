52afffee8cd6ba2d84d0ff0b5bdbccf8829d9583
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-12-16 03:07:23 +0100

    Add global styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Footer from './components/Footer';
+import { GlobalStyles } from './components/styled-components/GlobalStyles';
 
 export default function App() {
   return (
     <BrowserRouter>
+      <GlobalStyles />
       <Footer />
     </BrowserRouter>
   );
