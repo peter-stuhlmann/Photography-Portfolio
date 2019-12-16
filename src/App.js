7a785ae2afc1736c179776b7fab27d62cc644dec
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-12-16 03:18:16 +0100

    Add header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Footer from './components/Footer';
+import Header from './components/Header';
 import { GlobalStyles } from './components/styled-components/GlobalStyles';
 
 export default function App() {
   return (
     <BrowserRouter>
       <GlobalStyles />
+      <Header />
       <Footer />
     </BrowserRouter>
   );
