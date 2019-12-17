022bce68d92e282e8b3bb9913f6c8ade62c742fd
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-12-17 01:51:35 +0100

    Integrate rendered text from external file

---
+++ src/data/Text.js
@@ -0,0 +1,34 @@
+export const text = {
+  notFound: {
+    heading: 'Die Seite wurde nicht gefunden...',
+    list: {
+      heading: 'Suchst Du eine der folgenden Seiten?',
+      links: [
+        {
+          linkText: 'Portraits',
+          href: '/',
+        },
+        {
+          linkText: 'Akt & Erotik',
+          href: '/akt',
+        },
+        {
+          linkText: 'Landschafts- und Architekturfotografie',
+          href: '/landschaftsfotografie',
+        },
+        {
+          linkText: 'Über mich',
+          href: '/ueber-mich',
+        },
+        {
+          linkText: 'Blog',
+          href: '/blog',
+        },
+        {
+          linkText: 'Kontakt',
+          href: '/kontakt',
+        },
+      ],
+    },
+  },
+};
