3708f29b16e28cc5dc3116dc6e241c5770b28ee9
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-12-17 16:18:01 +0100

    Create empty privacy policy page
    Rename function name of legal notice component

---
+++ src/components/PrivacyPolicy.js
@@ -0,0 +1,12 @@
+import React, { Fragment, useContext } from 'react';
+import { Context } from '../Context';
+
+export default function PrivacyPolicy() {
+  const { text } = useContext(Context);
+
+  return (
+    <Fragment>
+      <h1>Datenschutzerklärung</h1>
+    </Fragment>
+  );
+}
