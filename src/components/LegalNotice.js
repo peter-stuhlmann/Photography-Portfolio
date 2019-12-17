4cc6c290a3235be450ded7a7a2829beebb62e2d3
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-12-17 06:46:25 +0100

    Add legal notice page

---
+++ src/components/LegalNotice.js
@@ -0,0 +1,25 @@
+import React, { Fragment, useContext } from 'react';
+import { Context } from '../Context';
+
+export default function About() {
+  const { text } = useContext(Context);
+
+  return (
+    <Fragment>
+      <h1>{text.legalNotice.heading}</h1>
+      <h2>{text.legalNotice.tmg.heading}</h2>
+      <p
+        dangerouslySetInnerHTML={{
+          __html: text.legalNotice.tmg.text,
+        }}
+      />
+      <h2>{text.legalNotice.disclaimerContent.heading}</h2>
+      <p>{text.legalNotice.disclaimerContent.text}</p>
+      <h2>{text.legalNotice.disclaimerLinks.heading}</h2>
+      <p>{text.legalNotice.disclaimerLinks.text}</p>
+      <h2>{text.legalNotice.copyright.heading}</h2>
+      <p>{text.legalNotice.copyright.text}</p>
+      <p>{text.legalNotice.source}</p>
+    </Fragment>
+  );
+}
--- src/data/Text.js
+++ src/data/Text.js
@@ -61,6 +61,30 @@ export const text = {
     text:
       'Mein Name ist Peter R. Stuhlmann und ich bin Online-Unternehmer und Fotograf mit Schwerpunkt in den Bereichen People-, Landschafts- und Architekturfotografie. Ich bin 23 Jahre alt und wohne in Berlin. Fotoshootings biete ich deutschlandweit, vorzugsweise aber im Raum Berlin an.<br />Neben dem Portfolio auf dieser Website findest Du meine fotografischen Arbeiten im Internet auch bei <a href="https://instagram.com/peter.stuhlmann.fotografie">Instagram</a>. Ich freue mich dort auf Deinen Besuch.',
   },
+  legalNotice: {
+    heading: 'Impressum',
+    tmg: {
+      heading: 'Angaben gemäß § 5 TMG',
+      text:
+        '<p>Peter R. Stuhlmann<br />Martha-Arendsee-Str. 10<br />12681 Berlin</p><p>Telefon: +49 (0) 1578 5630944<br />E-Mail: info@peter-stuhlmann.de</p><p> Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.</p>',
+    },
+    disclaimerContent: {
+      heading: 'Haftung für Inhalte',
+      text:
+        'Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.',
+    },
+    disclaimerLinks: {
+      heading: 'Haftung für Links',
+      text:
+        'Unser Angebot enthält Links zu externen Webseiten Dritter, auf deren Inhalte wir keinen Einfluss haben. Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten      verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der    Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte      waren zum Zeitpunkt der Verlinkung nicht erkennbar. Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete      Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.',
+    },
+    copyright: {
+      heading: 'Urheberrecht',
+      text:
+        'Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet. Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.',
+    },
+    source: 'Quelle: https://www.e-recht24.de',
+  },
   notFound: {
     heading: 'Die Seite wurde nicht gefunden...',
     list: {
