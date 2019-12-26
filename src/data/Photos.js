0ec86241c35ceca51834932f264fd1574448becf
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-12-26 19:37:39 +0100

    Undo the source of the images
    Fetch images from a javascript file instead of an API because of problems with the API.

---
+++ src/data/Photos.js
@@ -0,0 +1,23 @@
+export const landscapes = [
+  {
+    src: '***',
+    width: 3,
+    height: 2,
+  },
+];
+
+export const nudes = [
+  {
+    src: '***',
+    width: 3,
+    height: 2,
+  },
+];
+
+export const portraits = [
+  {
+    src: '***',
+    width: 3,
+    height: 2,
+  },
+];
