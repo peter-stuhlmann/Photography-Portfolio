a65c265c4ea64c19bb064b8561d96becf21d3d5d
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2020-01-30 05:25:18 +0100

    Store the portfolio galleries in separate files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { Fragment, useState, useCallback } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Gallery from 'react-photo-gallery';
-import Carousel, { Modal, ModalGateway } from 'react-images';
 import ContextProvider from './Context';
 import { GlobalStyles } from './components/styled-components/GlobalStyles';
 import { Wrapper } from './components/styled-components/Wrapper';
@@ -12,27 +10,16 @@ import Contact from './components/Contact';
 import NotFound from './components/NotFound';
 import LegalNotice from './components/LegalNotice';
 import PrivacyPolicy from './components/PrivacyPolicy';
-import { landscapes, nudes, portraits } from './data/Photos';
 import Prices from './components/Prices';
 import Footer from './components/Footer';
 import Sitemap from './components/Sitemap';
 import Blog from './components/Blog';
 import BlogPost from './components/BlogPost';
+import Portraits from './components/Portraits';
+import Nudes from './components/Nudes';
+import Landscapes from './components/Landscapes';
 
 export default function App() {
-  const [currentImage, setCurrentImage] = useState(0);
-  const [viewerIsOpen, setViewerIsOpen] = useState(false);
-
-  const openLightbox = useCallback((event, { index }) => {
-    setCurrentImage(index);
-    setViewerIsOpen(true);
-  }, []);
-
-  const closeLightbox = () => {
-    setCurrentImage(0);
-    setViewerIsOpen(false);
-  };
-
   return (
     <ContextProvider>
       <BrowserRouter>
@@ -40,87 +27,9 @@ export default function App() {
         <Header />
         <Wrapper>
           <Switch>
-            <Route
-              exact
-              path="/"
-              render={() =>
-                portraits ? (
-                  <Fragment>
-                    <Gallery photos={portraits} onClick={openLightbox} />
-                    <ModalGateway>
-                      {viewerIsOpen ? (
-                        <Modal onClose={closeLightbox}>
-                          <Carousel
-                            currentIndex={currentImage}
-                            views={portraits.map(x => ({
-                              ...x,
-                              srcset: x.srcSet,
-                              caption: x.title,
-                            }))}
-                          />
-                        </Modal>
-                      ) : null}
-                    </ModalGateway>
-                  </Fragment>
-                ) : (
-                  'Bilder werden geladen ...'
-                )
-              }
-            />
-            <Route
-              exact
-              path="/akt"
-              render={() =>
-                nudes ? (
-                  <Fragment>
-                    <Gallery photos={nudes} onClick={openLightbox} />
-                    <ModalGateway>
-                      {viewerIsOpen ? (
-                        <Modal onClose={closeLightbox}>
-                          <Carousel
-                            currentIndex={currentImage}
-                            views={nudes.map(x => ({
-                              ...x,
-                              srcset: x.srcSet,
-                              caption: x.title,
-                            }))}
-                          />
-                        </Modal>
-                      ) : null}
-                    </ModalGateway>
-                  </Fragment>
-                ) : (
-                  'Bilder werden geladen ...'
-                )
-              }
-            />
-            <Route
-              exact
-              path="/landschaftsfotografie"
-              render={() =>
-                landscapes ? (
-                  <Fragment>
-                    <Gallery photos={landscapes} onClick={openLightbox} />
-                    <ModalGateway>
-                      {viewerIsOpen ? (
-                        <Modal onClose={closeLightbox}>
-                          <Carousel
-                            currentIndex={currentImage}
-                            views={landscapes.map(x => ({
-                              ...x,
-                              srcset: x.srcSet,
-                              caption: x.title,
-                            }))}
-                          />
-                        </Modal>
-                      ) : null}
-                    </ModalGateway>
-                  </Fragment>
-                ) : (
-                  'Bilder werden geladen ...'
-                )
-              }
-            />
+            <Route exact path="/" component={Portraits} />
+            <Route exact path="/akt" component={Nudes} />
+            <Route exact path="/landschaftsfotografie" component={Landscapes} />
             <Route exact path="/blog" component={Blog} />
             <Route exact path="/blog/:slug" component={BlogPost} />
             <Route exact path="/kontakt" component={Contact} />
