7f2ee7e4e63c65152e0e1f2e10b6cc6093aefc22
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2020-02-13 13:38:38 +0100

    Add styling for testimonials section

--- src/components/About.js
+++ src/components/About.js
@@ -41,9 +41,11 @@ export default function About() {
       <h1>{text.testimonials.heading}</h1>
       {text.testimonials.cards.map(cards => (
         <Testimonials key={cards.img}>
-          <img src={cards.img} alt={cards.name} />
+          <div>
+            <img src={cards.img} alt={cards.name} />
+          </div>
           <p>
-            {cards.quote} <span>{cards.name}</span>
+            <span>{cards.name}</span> {cards.quote} <span>{cards.name}</span>
           </p>
         </Testimonials>
       ))}
@@ -51,4 +53,66 @@ export default function About() {
   );
 }
 
-const Testimonials = styled.div``;
+const Testimonials = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  margin-bottom: 45px;
+
+  @media screen and (min-width: 700px) {
+    margin-bottom: 25px;
+  }
+
+  div {
+    flex: 0 0 100%;
+    text-align: center;
+
+    @media screen and (min-width: 700px) {
+      flex: 0 0 150px;
+      text-align: left;
+    }
+
+    img {
+      border-radius: 50%;
+      margin-right: 0;
+      width: 150px;
+
+      @media screen and (min-width: 700px) {
+        margin-right: 35px;
+      }
+    }
+  }
+
+  p {
+    display: 
+    flex: 0 0 100%;
+    margin: 0;
+
+    @media screen and (min-width: 700px) {
+      flex: 0 0 calc(100% - 150px - 35px);
+    }
+  }
+
+  span {
+    // mobile screens < 700px
+    &:first-child {
+      display: block;
+      text-align: center;
+      font-style: italic;
+
+      @media screen and (min-width: 700px) {
+        display: none;
+      }
+    }
+
+    // desktop screens >= 700px
+    &:last-child {
+      display: none;
+      font-style: italic;
+      float: right;
+
+      @media screen and (min-width: 700px) {
+        display: inline-block;
+      }
+    }
+  }
+`;
