88b0c7f640aebd835e21e8de3f955210738ed9b1
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2020-03-19 03:44:03 +0100

    Create voucher page

---
+++ src/components/Vouchers.js
@@ -0,0 +1,29 @@
+import React, { Fragment, useContext } from 'react';
+
+import { Context } from '../Context';
+import { Image } from './styled-components/Image';
+import { TwoColumns } from './styled-components/Columns';
+
+export default function Vouchers() {
+  const { text } = useContext(Context);
+
+  return (
+    <Fragment>
+      <h1>{text.vouchers.heading}</h1>
+      <TwoColumns columnLeft="0 0 48%" columnRight="0 0 48%">
+        <div className="column-left">
+          <p>
+            {text.vouchers.content[0]}
+            <a title={text.vouchers.link.title} href={text.vouchers.link.href}>
+              {text.vouchers.link.linkText}
+            </a>
+            {text.vouchers.content[1]}
+          </p>
+        </div>
+        <div className="column-right">
+          <Image src={text.vouchers.image.src} alt={text.vouchers.image.alt} />
+        </div>
+      </TwoColumns>
+    </Fragment>
+  );
+}
