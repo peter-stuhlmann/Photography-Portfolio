5deacdbf42200d85729a71a8a7c632911dff9c14
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2020-04-07 14:12:17 +0200

    Add loading spinner to portfolio gallery images

--- src/components/styled-components/GlobalStyles.js
+++ src/components/styled-components/GlobalStyles.js
@@ -1,11 +1,13 @@
 import { createGlobalStyle } from 'styled-components';
 import RalewayRegular from '../../assets/fonts/raleway/Raleway-Regular.ttf';
+import Spinner from '../../assets/img/spinner.svg';
 import { textColor, backgroundColor } from './Themes';
 
 export const GlobalStyles = createGlobalStyle`
   @font-face {
     font-family: 'Raleway';
     src: url(${RalewayRegular});
+    font-display: block;
   }
 
   body {
@@ -20,6 +22,23 @@ export const GlobalStyles = createGlobalStyle`
     .react-photo-gallery--gallery img {
       filter: grayscale(100%);
       transition: 0.5s;
+      background-color: #969696;
+      background-image: url(${Spinner});
+      background-size: 100px 100px;
+      background-repeat: no-repeat;
+      background-position: center;
+
+      &::after {
+        content: "";
+        background: ${backgroundColor};
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        z-index: -1
+      }
+      
 
       &:hover {
         filter: grayscale(0%);
