7434a78bfa7a29a5f83602b78fa9866c83e11ba1
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2020-04-12 14:03:43 +0200

    Add some more routes and sort all by name

--- src/components/Router.js
+++ src/components/Router.js
@@ -4,49 +4,63 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import { ga } from '../helpers/analytics';
 
 const About = lazy(() => import('./pages/About'));
-const Contact = lazy(() => import('./pages/Contact'));
-const NotFound = lazy(() => import('./pages/NotFound'));
-const LegalNotice = lazy(() => import('./pages/LegalNotice'));
-const PrivacyPolicy = lazy(() => import('./pages/PrivacyPolicy'));
-const Prices = lazy(() => import('./pages/Prices'));
-const Sitemap = lazy(() => import('./pages/Sitemap'));
+const Affiliate = lazy(() => import('./pages/Affiliate'));
 const Blog = lazy(() => import('./pages/Blog'));
 const BlogPost = lazy(() => import('./pages/BlogPost'));
-const PortfolioGallery = lazy(() => import('./pages/PortfolioGallery'));
-const PhotoAPI = lazy(() => import('./pages/PhotoAPI'));
-const GeneralTerms = lazy(() => import('./pages/GeneralTerms'));
-const Vouchers = lazy(() => import('./pages/Vouchers'));
 const Calendar = lazy(() => import('./pages/Calendar'));
+const Contact = lazy(() => import('./pages/Contact'));
+const Course = lazy(() => import('./pages/Course'));
 const Downloads = lazy(() => import('./pages/Downloads'));
+const Faq = lazy(() => import('./pages/Faq'));
+const GeneralTerms = lazy(() => import('./pages/GeneralTerms'));
+const LegalNotice = lazy(() => import('./pages/LegalNotice'));
+const NotFound = lazy(() => import('./pages/NotFound'));
+const PhotoAPI = lazy(() => import('./pages/PhotoAPI'));
+const PortfolioGallery = lazy(() => import('./pages/PortfolioGallery'));
+const Prices = lazy(() => import('./pages/Prices'));
+const Prints = lazy(() => import('./pages/Prints'));
+const PrivacyPolicy = lazy(() => import('./pages/PrivacyPolicy'));
+const RightOfWithdrawal = lazy(() => import('./pages/RightOfWithdrawal'));
+const Sitemap = lazy(() => import('./pages/Sitemap'));
+const Vouchers = lazy(() => import('./pages/Vouchers'));
 
 export default function Router() {
   return (
     <Suspense fallback={<div>Inhalt wird geladen...</div>}>
       <Switch>
         <Route exact path="/" component={ga(PortfolioGallery)} />
-        <Route exact path="/portraits" render={() => <Redirect to="/" />} />
+        <Route exact path="/agb" component={ga(GeneralTerms)} />
+        <Route exact path="/ablauf" component={ga(Course)} />
         <Route exact path="/akt" component={ga(PortfolioGallery)} />
+        <Route exact path="/api" component={ga(PhotoAPI)} />
+        <Route exact path="/blog" component={ga(Blog)} />
+        <Route exact path="/blog/:slug" component={ga(BlogPost)} />
+        <Route
+          exact
+          path="/datenschutzerklaerung"
+          component={ga(PrivacyPolicy)}
+        />
+        <Route exact path="/downloads" component={ga(Downloads)} />
+        <Route exact path="/faq" component={ga(Faq)} />
+        <Route exact path="/gutscheine" component={ga(Vouchers)} />
+        <Route exact path="/impressum" component={ga(LegalNotice)} />
+        <Route exact path="/kalender" component={ga(Calendar)} />
+        <Route exact path="/kontakt" component={ga(Contact)} />
         <Route
           exact
           path="/landschaftsfotografie"
           component={ga(PortfolioGallery)}
         />
-        <Route exact path="/blog" component={ga(Blog)} />
-        <Route exact path="/blog/:slug" component={ga(BlogPost)} />
-        <Route exact path="/kontakt" component={ga(Contact)} />
+        <Route exact path="/partnerprogramm" component={ga(Affiliate)} />
+        <Route exact path="/portraits" render={() => <Redirect to="/" />} />
         <Route exact path="/preise" component={ga(Prices)} />
-        <Route exact path="/downloads" component={ga(Downloads)} />
-        <Route exact path="/ueber-mich" component={ga(About)} />
+        <Route exact path="/prints" component={ga(Prints)} />
         <Route exact path="/sitemap" component={ga(Sitemap)} />
-        <Route exact path="/api" component={ga(PhotoAPI)} />
-        <Route exact path="/gutscheine" component={ga(Vouchers)} />
-        <Route exact path="/kalender" component={ga(Calendar)} />
-        <Route exact path="/agb" component={ga(GeneralTerms)} />
-        <Route exact path="/impressum" component={ga(LegalNotice)} />
+        <Route exact path="/ueber-mich" component={ga(About)} />
         <Route
           exact
-          path="/datenschutzerklaerung"
-          component={ga(PrivacyPolicy)}
+          path="/widerrufsbelehrung"
+          component={ga(RightOfWithdrawal)}
         />
         <Route component={ga(NotFound)} />
       </Switch>
