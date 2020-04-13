98245a998b0bf19b441f27a80e7d2d2f6d6d7526
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2020-04-13 20:13:08 +0200

    Return different elements (a or Link) depending on props

--- src/components/styled-components/Buttons.js
+++ src/components/styled-components/Buttons.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 import {
   buttonBackgroundColor,
@@ -8,25 +9,29 @@ import {
 import { Pdf } from '../Icons';
 
 export const ButtonLink = props => {
-  const { href, linkText, pdf, title, style } = props;
+  const { external, href, linkText, title, style } = props;
 
-  return (
-    <StyledButtonLink
+  return external || href.substring(href.length - 4) === '.pdf' ? (
+    <StyledButtonExternalLink
       href={href}
       style={style}
       title={title}
       target="_blank"
       rel="noopener noreferrer"
     >
-      {pdf ? <Pdf /> : null} {linkText}
+      {href.substring(href.length - 4) === '.pdf' ? <Pdf /> : null} {linkText}
+    </StyledButtonExternalLink>
+  ) : (
+    <StyledButtonLink to={href} style={style} title={title}>
+      {linkText}
     </StyledButtonLink>
   );
 };
 
-const StyledButtonLink = styled.a`
+const StyledButtonLink = styled(Link)`
   background-color: ${buttonBackgroundColor};
   border-radius: 5px;
-  color: ${buttonTextColor};
+  color: ${buttonTextColor} !important;
   padding: 7px 15px;
   text-decoration: none;
 
@@ -35,5 +40,20 @@ const StyledButtonLink = styled.a`
     height: 16px;
     margin-bottom: -2px
     margin-right: 10px;
+  }
+`;
+
+const StyledButtonExternalLink = styled.a`
+  background-color: ${buttonBackgroundColor};
+  border-radius: 5px;
+  color: ${buttonTextColor} !important;
+  padding: 7px 15px;
+  text-decoration: none;
+
+  svg {
+    fill: ${buttonTextColor};
+    height: 16px;
+    margin-bottom: -2px
+    margin-right: 5px;
   }
 `;
