79baa1b48b78f2230dfa54138ea3dd92f2d0a6cd
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2020-12-28 00:17:48 +0100

    Optimize gallery (lazy load)

--- src/components/pages/PortfolioGallery.js
+++ src/components/pages/PortfolioGallery.js
@@ -3,15 +3,13 @@ import { useInView } from 'react-intersection-observer';
 import Gallery from 'react-photo-gallery';
 import Carousel, { Modal, ModalGateway } from 'react-images';
 
-import { debounce } from '../../helpers/debounce';
 import { meta } from '../../helpers/meta';
-
-import loading from '../../data/loading';
-
 import portraitsPage from '../../data/portraits';
 import landscapesPage from '../../data/landscapes';
 import nudesPage from '../../data/nudes';
 import { portraits, landscapes, nudes } from '../../data/Photos';
+import pixel from '../../assets/img/pixel.svg';
+import loading from '../../data/loading';
 
 export default function PortfolioGallery() {
   const route = window.location.pathname;
@@ -50,29 +48,45 @@ export default function PortfolioGallery() {
     setViewerIsOpen(false);
   };
 
-  const [images, setImages] = useState(photos.slice(0, 6));
-  const [amount, setAmount] = useState(6);
-  const [loadedAll, setLoadedAll] = useState(false);
+  const [amount, setAmount] = useState(1);
 
   const [ref, inView] = useInView();
 
-  const loadMorePhotos = debounce(() => {
-    if (amount > photos.length) {
-      setLoadedAll(true);
-    }
-    setImages(images.concat(photos.slice(images.length, images.length + 6)));
-    setAmount(amount + 6);
-  }, 200);
+  const loadMorePhotos = () => {
+    setAmount(amount + 2);
+  };
 
   useEffect(() => {
-    if (inView === true) {
+    if (inView === true && !(amount >= photos.length)) {
       loadMorePhotos();
     }
   });
 
+  const [delayed, setDelayed] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      setDelayed(true);
+    }, 1300);
+  }, [delayed]);
+
+  const images = photos.map((image, index) => {
+    return {
+      alt: image.alt,
+      title: image.title,
+      height: image.height,
+      width: image.width,
+      src: amount > index ? image.src : pixel,
+      key: image.src,
+      ref: index < amount ? ref : null,
+    };
+  });
+
   return (
     <Fragment>
-      <Gallery photos={images} onClick={openLightbox} />
+      {delayed === false && <p>{loading.images}</p>}
+      <div style={delayed ? { opacity: '1' } : { opacity: '0' }}>
+        <Gallery photos={images} onClick={openLightbox} />
+      </div>
       <ModalGateway>
         {viewerIsOpen ? (
           <Modal onClose={closeLightbox}>
@@ -87,7 +101,6 @@ export default function PortfolioGallery() {
           </Modal>
         ) : null}
       </ModalGateway>
-      {!loadedAll && <div ref={ref}>{loading.images}</div>}
     </Fragment>
   );
 }
--- src/components/styled-components/GlobalStyles.js
+++ src/components/styled-components/GlobalStyles.js
@@ -1,6 +1,5 @@
 import { createGlobalStyle } from 'styled-components';
 import RalewayRegular from '../../assets/fonts/raleway/Raleway-Regular.ttf';
-import Spinner from '../../assets/img/spinner.svg';
 import { textColor, backgroundColor } from './Themes';
 
 export const GlobalStyles = createGlobalStyle`
@@ -22,6 +21,7 @@ export const GlobalStyles = createGlobalStyle`
     line-height: 1.6;
     margin: 0;
     transition: 0.3s;
+    overflow-y: scroll;
 
     .external-inline-link,
     .internal-inline-link {
@@ -37,23 +37,7 @@ export const GlobalStyles = createGlobalStyle`
     .react-photo-gallery--gallery img {
       filter: grayscale(100%);
       transition: 0.5s;
-      background-color: #969696;
-      background-image: url(${Spinner});
-      background-size: 100px 100px;
-      background-repeat: no-repeat;
-      background-position: center;
-
-      &::after {
-        content: "";
-        background: ${backgroundColor};
-        position: absolute;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        z-index: -1
-      }
-      
+      background-color: #969696;    
 
       &:hover {
         filter: grayscale(0%);
