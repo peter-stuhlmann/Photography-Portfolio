29e80cd7c7face656898f84874cf88b5ce7e651d
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2021-08-24 21:39:17 +0200

    Fix bug: creating a meta description depending on page

--- src/helpers/meta.js
+++ src/helpers/meta.js
@@ -1,6 +1,5 @@
 export function meta(attribute, name, content) {
-  const meta = document.createElement('meta');
-  meta.setAttribute(attribute, name);
-  meta.content = content;
-  document.getElementsByTagName('head')[0].appendChild(meta);
+  document
+    .querySelector('meta[' + attribute + '=' + name + ']')
+    .setAttribute('content', content);
 }
